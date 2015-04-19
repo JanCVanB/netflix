2e3adc64acab4d8137ee4e93222bc880214182b0
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-04-18 19:10:48 -0700

    Simplify svd.run(ratings) to only call svd.train(ratings)

--- algorithms/svd.py
+++ algorithms/svd.py
@@ -1,15 +1,9 @@
 import numpy as np
 
 
-def run(input_array=None):
-    if input_array is None:
-        return run_all()
-    return run_custom(input_array)
+def run(ratings):
+    return train(ratings)
 
 
-def run_all():
-    return np.array([])
-
-
-def run_custom(input_array):
-    return np.array([])
+def train(ratings):
+    pass
--- tests/test_svd.py
+++ tests/test_svd.py
@@ -3,7 +3,7 @@ import numpy as np
 from algorithms import svd
 
 
-def help_get_simple_ratings_array():
+def help_get_simple_ratings():
     ratings = ((1, 0, 2),
                (0, 3, 0),
                (4, 0, 5),
@@ -11,40 +11,13 @@ def help_get_simple_ratings_array():
     return np.array(ratings)
 
 
-def test_run_can_accept_simple_ratings_as_only_argument():
-    simple_ratings_array = help_get_simple_ratings_array()
-    svd.run(simple_ratings_array)
-
-
-def test_run_on_no_arguments_returns_numpy_array():
-    output_array = svd.run()
-    assert isinstance(output_array, np.ndarray)
-
-
-def test_run_on_no_arguments_returns_result_of_run_all(monkeypatch):
+def test_run_returns_result_of_train_on_ratings(monkeypatch):
     from random import random
     unique_value = random()
-    dummy_run_all = lambda: unique_value
-    monkeypatch.setattr(svd, 'run_all', dummy_run_all)
+    dummy_train = lambda ratings: unique_value
+    monkeypatch.setattr(svd, 'train', dummy_train)
 
-    expected_result = svd.run_all()
-    actual_result = svd.run()
-    assert actual_result == expected_result
-
-
-def test_run_on_simple_ratings_returns_numpy_array():
-    simple_ratings_array = help_get_simple_ratings_array()
-    output_array = svd.run(simple_ratings_array)
-    assert isinstance(output_array, np.ndarray)
-
-
-def test_run_on_simple_ratings_returns_result_of_run_custom(monkeypatch):
-    from random import random
-    unique_value = random()
-    dummy_run_custom = lambda ratings: unique_value
-    monkeypatch.setattr(svd, 'run_custom', dummy_run_custom)
-
-    simple_ratings_array = help_get_simple_ratings_array()
-    expected_result = svd.run_custom(simple_ratings_array)
-    actual_result = svd.run(simple_ratings_array)
+    simple_ratings = help_get_simple_ratings()
+    expected_result = svd.train(simple_ratings)
+    actual_result = svd.run(simple_ratings)
     assert actual_result == expected_result
