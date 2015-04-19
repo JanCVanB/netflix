05212b5bddb339da689dfa49b67842a8fd5da9e5
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-04-19 12:23:44 -0700

    Improve svd.update_feature() by iterating training points correctly

--- algorithms/svd.py
+++ algorithms/svd.py
@@ -18,29 +18,49 @@ class SVD(Model):
         self.num_users = 0
         self.num_movies = 0
 
-    def calculate_prediction_error(self):
-        pass
+    def calculate_num_movies(self):
+        return np.amax(self.ratings[:, 1]) + 1
+
+    def calculate_num_users(self):
+        return np.amax(self.ratings[:, 0]) + 1
+
+    def calculate_prediction_error(self, user, movie, rating):
+        return rating - np.dot(self.users[user, :], self.movies[:, movie])
 
     def initialize_users_and_movies(self):
-        self.num_users, self.num_movies = self.ratings.shape
+        self.num_users = self.calculate_num_users()
+        self.num_movies = self.calculate_num_movies()
         self.users = np.full((self.num_users, self.num_features),
                              self.feature_initial)
         self.movies = np.full((self.num_features, self.num_movies),
                               self.feature_initial)
 
-    def train(self, ratings, epochs=2):
+    def iterate_training_points(self):
+        for point in self.ratings:
+            if point[3] > 0:
+                yield point
+
+    def set_ratings(self, ratings):
         self.ratings = ratings
+
+    def train(self, ratings, epochs=2):
+        self.set_ratings(ratings)
         self.initialize_users_and_movies()
         for _ in range(epochs):
             self.update_features()
 
     def update_feature(self, feature):
-        self.calculate_prediction_error()
-        for user in range(self.num_users):
-            self.update_user(user)
-        for movie in range(self.num_movies):
-            self.update_movie(movie)
+        for user, movie, _, rating in self.iterate_training_points():
+            error = self.calculate_prediction_error(user, movie, rating)
+            self.update_user(user, movie, feature, error)
+            self.update_movie(user, movie, feature, error)
 
     def update_features(self):
         for feature in range(self.num_features):
             self.update_feature(feature)
+
+    def update_movie(self, user, movie, feature, error):
+        pass
+
+    def update_user(self, user, movie, feature, error):
+        pass
--- tests/test_svd.py
+++ tests/test_svd.py
@@ -4,11 +4,20 @@ from unittest.mock import call, Mock
 from algorithms import svd
 
 
-def help_get_simple_ratings():
-    ratings = ((1, 0, 2),
-               (0, 3, 0),
-               (4, 0, 5),
-               (0, 1, 0))
+MockThatAvoidsErrors = Mock
+MockThatAvoidsLongRunTime = Mock
+MockThatTracksCallsWithoutRunning = Mock
+
+
+def make_simple_ratings():
+    ratings = ((1, 2, 0, 1),
+               (3, 4, 0, 2),
+               (5, 1, 0, 3),
+               (2, 3, 0, 4),
+               (4, 5, 0, 5),
+               (1, 3, 0, 0),
+               (5, 2, 0, 1),
+               (4, 1, 0, 2))
     return np.array(ratings)
 
 
@@ -62,30 +71,47 @@ def test_svd_sets_custom_user_feature_init_value():
     assert model.feature_initial == custom_feature_initial_value
 
 
+def test_svd_set_ratings_sets_ratings_to_expected_matrix():
+    model = svd.SVD()
+    simple_ratings = make_simple_ratings()
+    model.set_ratings(simple_ratings)
+    np.testing.assert_array_equal(model.ratings, simple_ratings)
+
+
+def test_svd_train_sets_ratings():
+    model = svd.SVD()
+    simple_ratings = make_simple_ratings()
+    model.initialize_users_and_movies = MockThatAvoidsErrors()
+    model.update_features = MockThatAvoidsLongRunTime()
+    model.set_ratings = MockThatTracksCallsWithoutRunning()
+    model.train(simple_ratings)
+    assert model.set_ratings.call_count == 1
+
+
 def test_svd_train_initializes_user_and_movie_feature_matrices():
     model = svd.SVD()
-    simple_ratings = help_get_simple_ratings()
-    model.initialize_users_and_movies = Mock()
-    model.update_features = Mock()
+    simple_ratings = make_simple_ratings()
+    model.update_features = MockThatAvoidsLongRunTime()
+    model.initialize_users_and_movies = MockThatTracksCallsWithoutRunning()
     model.train(simple_ratings)
     assert model.initialize_users_and_movies.call_count == 1
 
 
 def test_svd_train_updates_features_the_expected_number_of_times():
     model = svd.SVD()
-    simple_ratings = help_get_simple_ratings()
+    simple_ratings = make_simple_ratings()
     number_of_epochs = 3
-    model.update_features = Mock()
+    model.update_features = MockThatTracksCallsWithoutRunning()
     model.train(simple_ratings, epochs=number_of_epochs)
     assert model.update_features.call_count == number_of_epochs
 
 
 def test_svd_initialize_users_and_movies_sets_expected_num_users_and_movies():
-    simple_ratings = help_get_simple_ratings()
     model = svd.SVD()
-    model.update_features = Mock()
-    model.train(simple_ratings)
-    expected_num_users, expected_num_movies = simple_ratings.shape
+    simple_ratings = make_simple_ratings()
+    model.set_ratings(simple_ratings)
+    expected_num_users = model.calculate_num_users()
+    expected_num_movies = model.calculate_num_movies()
     model.initialize_users_and_movies()
     actual_num_users = model.num_users
     actual_num_movies = model.num_movies
@@ -93,14 +119,15 @@ def test_svd_initialize_users_and_movies_sets_expected_num_users_and_movies():
     np.testing.assert_array_equal(actual_num_movies, expected_num_movies)
 
 
-def test_svd_initialize_users_and_movies_sets_expected_matrices():
-    simple_ratings = help_get_simple_ratings()
+def test_svd_initialize_users_and_movies_sets_expected_users_movies_matrices():
     model = svd.SVD()
-    model.update_features = Mock()
-    model.train(simple_ratings)
-    expected_users = np.full((model.num_users, model.num_features),
+    simple_ratings = make_simple_ratings()
+    model.set_ratings(simple_ratings)
+    num_users = model.calculate_num_users()
+    num_movies = model.calculate_num_movies()
+    expected_users = np.full((num_users, model.num_features),
                              model.feature_initial)
-    expected_movies = np.full((model.num_features, model.num_movies),
+    expected_movies = np.full((model.num_features, num_movies),
                               model.feature_initial)
     model.initialize_users_and_movies()
     actual_users = model.users
@@ -109,39 +136,101 @@ def test_svd_initialize_users_and_movies_sets_expected_matrices():
     np.testing.assert_array_equal(actual_movies, expected_movies)
 
 
-def test_update_features_updates_each_feature_once_in_any_order():
+def initialize_model_with_simple_ratings(model):
+    simple_ratings = make_simple_ratings()
+    model.set_ratings(make_simple_ratings())
+    model.initialize_users_and_movies()
+    return simple_ratings
+
+
+def test_svd_calculate_num_users_returns_expected_number():
     model = svd.SVD()
-    model.update_feature = Mock()
+    simple_ratings = initialize_model_with_simple_ratings(model)
+    expected_num_users = np.amax(simple_ratings[:, 0]) + 1
+    actual_num_users = model.calculate_num_users()
+    assert actual_num_users == expected_num_users
+
+
+def test_svd_calculate_num_movies_returns_expected_number():
+    model = svd.SVD()
+    simple_ratings = initialize_model_with_simple_ratings(model)
+    expected_num_movies = np.amax(simple_ratings[:, 1]) + 1
+    actual_num_movies = model.calculate_num_movies()
+    assert actual_num_movies == expected_num_movies
+
+
+def test_svd_update_features_updates_each_feature_once_in_any_order():
+    model = svd.SVD()
+    model.update_feature = MockThatTracksCallsWithoutRunning()
     model.update_features()
     assert model.update_feature.call_count == model.num_features
     expected_calls = [call(f) for f in range(model.num_features)]
     model.update_feature.assert_has_calls(expected_calls, any_order=True)
 
 
-def test_update_feature_calculates_prediction_error():
+def test_svd_update_feature_calculates_prediction_error_at_least_once():
     model = svd.SVD()
+    initialize_model_with_simple_ratings(model)
     model.calculate_prediction_error = Mock()
     model.update_user = Mock()
     model.update_movie = Mock()
     some_feature = 0
     model.update_feature(some_feature)
-    assert model.calculate_prediction_error.call_count == 1
+    assert model.calculate_prediction_error.call_count >= 1
 
 
-def test_update_feature_updates_each_user_and_movie_once_in_any_order():
+def test_svd_update_feature_updates_user_movie_for_each_train_point_any_order():
     model = svd.SVD()
-    simple_ratings = help_get_simple_ratings()
-    model.update_features = Mock()
-    model.train(simple_ratings)
+    initialize_model_with_simple_ratings(model)
     model.update_user = Mock()
     model.update_movie = Mock()
     some_feature = 0
     model.update_feature(some_feature)
-    assert model.update_user.call_count == model.num_users
-    assert model.update_movie.call_count == model.num_movies
-    expected_update_user_calls = [call(u) for u in range(model.num_users)]
-    expected_update_movie_calls = [call(m) for m in range(model.num_movies)]
-    model.update_user.assert_has_calls(expected_update_user_calls,
-                                       any_order=True)
-    model.update_movie.assert_has_calls(expected_update_movie_calls,
-                                        any_order=True)
+    training_points = list(model.iterate_training_points())
+    expected_num_calls = len(training_points)
+    assert model.update_user.call_count == expected_num_calls
+    assert model.update_movie.call_count == expected_num_calls
+    expected_calls = [call(user, movie, some_feature,
+                           model.calculate_prediction_error(user, movie, rating)
+                           )
+                      for user, movie, _, rating in training_points]
+    model.update_user.assert_has_calls(expected_calls, any_order=True)
+    model.update_movie.assert_has_calls(expected_calls, any_order=True)
+
+
+def sort_first_then_second(iterable):
+    return sorted(sorted(iterable, key=lambda x: x[1]), key=lambda x: x[0])
+
+
+def assert_lists_of_tuples_are_equal(a, b):
+    a = sort_first_then_second(a)
+    b = sort_first_then_second(b)
+    assert len(a) == len(b)
+    assert all(all(a[i][j] == b[i][j]
+                   for j in range(len(a[i])))
+               for i in range(len(a)))
+
+
+def test_svd_iterate_training_points_generates_expected_points():
+    model = svd.SVD()
+    initialize_model_with_simple_ratings(model)
+    expected_training_points = []
+    for point in model.ratings:
+        rating = point[3]
+        if rating > 0:
+            expected_training_points.append(point)
+    actual_training_points = model.iterate_training_points()
+    assert_lists_of_tuples_are_equal(actual_training_points,
+                                     expected_training_points)
+
+
+def test_svd_calculate_prediction_error_returns_expected_error():
+    model = svd.SVD()
+    simple_ratings = initialize_model_with_simple_ratings(model)
+    some_user, some_movie, _, some_rating = simple_ratings[0, :]
+    assert some_rating > 0
+    expected_error = (some_rating - np.dot(model.users[some_user, :],
+                                           model.movies[:, some_movie]))
+    actual_error = model.calculate_prediction_error(some_user, some_movie,
+                                                    some_rating)
+    assert actual_error == expected_error
