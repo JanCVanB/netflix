4ef7a85227c2cea4fa8e1e9bcd63658be6f4efe0
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-04-18 17:22:54 -0700

    Add svd.run_all() and svd.run_custom()

--- algorithms/svd.py
+++ algorithms/svd.py
@@ -3,3 +3,11 @@ import numpy as np
 
 def run(input_array=None):
     return np.array([])
+
+
+def run_all():
+    return np.array([])
+
+
+def run_custom(input_array):
+    return np.array([])
--- tests/test_svd.py
+++ tests/test_svd.py
@@ -1,12 +1,36 @@
+import numpy as np
+
+
+def help_get_simple_ratings_array():
+    ratings = ((1, 0, 2),
+               (0, 3, 0),
+               (4, 0, 5),
+               (0, 1, 0))
+    return np.array(ratings)
+
+
+def test_run_can_accept_simple_ratings_array_as_only_argument():
+    from algorithms.svd import run
+    simple_ratings_array = help_get_simple_ratings_array()
+    run(simple_ratings_array)
+
+
 def test_run_returns_numpy_array():
-    import numpy as np
     from algorithms.svd import run
     output_array = run()
     assert isinstance(output_array, np.ndarray)
 
 
-def test_run_can_accept_numpy_array_as_only_argument():
-    import numpy as np
-    from algorithms.svd import run
-    input_array = np.array([])
-    run(input_array)
+def test_run_with_no_arguments_returns_result_of_run_all():
+    from algorithms.svd import run, run_all
+    expected_array = run_all()
+    output_array = run()
+    np.testing.assert_array_equal(output_array, expected_array)
+
+
+def test_run_with_numpy_array_argument_returns_result_of_run_custom():
+    from algorithms.svd import run, run_custom
+    simple_ratings_array = help_get_simple_ratings_array()
+    expected_array = run_custom(simple_ratings_array)
+    output_array = run(simple_ratings_array)
+    np.testing.assert_array_equal(output_array, expected_array)
