4f169835b84154c9e322150018cee6ca914ce559
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-04-19 22:31:15 -0700

    Ignore MODELS_DIR_PATH and remove references (each model is ~70MB)

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,10 +7,10 @@ def test_model_can_create_instance_with_no_arguments():
 
 def test_model_save_creates_file_at_expected_file_path():
     import os.path
-    from utils.data_paths import MODELS_DIR_PATH
+    from utils.data_paths import ROOT_DIR_PATH
     model = Model()
     model_file_name = 'test'
-    model_file_path = os.path.join(MODELS_DIR_PATH, model_file_name)
+    model_file_path = os.path.join(ROOT_DIR_PATH, model_file_name)
     assertion_message = '%s is for test use only' % model_file_path
     assert not os.path.isfile(model_file_path), assertion_message
 
@@ -27,9 +27,9 @@ def test_model_save_creates_file_at_expected_file_path():
 
 def test_model_load_returns_none():
     from os.path import join
-    from utils.data_paths import MODELS_DIR_PATH
+    from utils.data_paths import ROOT_DIR_PATH
     model = Model()
     model_file_name = 'test'
-    model_file_path = join(MODELS_DIR_PATH, model_file_name)
+    model_file_path = join(ROOT_DIR_PATH, model_file_name)
     return_value = model.load(model_file_path)
     assert return_value is None
