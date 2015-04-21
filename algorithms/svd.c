06aa4012baa87f420745752249a1330286e3a1ab
qosha1 <qosha@caltech.edu>
2015-04-21 13:23:48 -0700

    Fix matrix pointer issues

--- algorithms/svd.c
+++ algorithms/svd.c
@@ -1,33 +1,37 @@
 int py_c_update_feature(int *train_points, int num_points, float *users, 
-	int num_users, float *movies, int num_movies, float learn_rate, int feature){
+	int num_users, float *movies, int num_movies, float learn_rate, 
+	int feature, int num_features){
 	
 	int i, j;
 	float temp_prediction;
+	int *temp_user_val, *temp_movie_val;
 	float error, user_change, movie_change;
 	int *user,*movie,*time,*rating;
 	int *train_cursor = train_points;
 	
 	for(i = 0; i < num_points; i++){
 		/* Get current variables   */
-		user   = (&train_points++;
-		movie  = &train_points++;
-		time   = &train_points++;
-		rating = &train_points++; 
+		user   = train_cursor++;
+		movie  = train_cursor++;
+		time   = train_cursor++;
+		rating = train_cursor++; 
 		
 		/* Calculate prediction error */
 		/* TODO: Use residuals to avoid repeat dot product calculations */
+		temp_user_val  = &(users[*user * num_features]);
+		temp_movie_val = &(movies[*movie * num_features]);
 		for(j = 0; j < num_features; j++){
-			temp_prediction += users[*user][j] * movies[j][*movie];
+			temp_prediction += temp_user_val[j] * temp_movie_val[j];
 			
 		}
 		
 		error = rating - temp_prediction;
 		/* Update user and movie */
-		user_change = learn_rate * error * movies[feature][*movie];
-		movie_change = learn_rate * error * users[*user][feature];
+		user_change  = learn_rate * error * movies[(*movie * num_features) + feature];
+		movie_change = learn_rate * error * users[(*user  * num_features) + feature];
 		
-		users[*user][feature]   += user_change;
-		movies[feature][*movie] += movie_change;
+		users[(*movie * num_features) + feature]   += user_change;
+		movies[(*user  * num_features) + feature]  += movie_change;
 		/* */
 	}
 }
