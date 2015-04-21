b0d7cb19bf4dec215ee711c6cad7c403095726bb
qosha1 <qosha@caltech.edu>
2015-04-21 13:38:00 -0700

    Fix dot product calculations

--- algorithms/svd.c
+++ algorithms/svd.c
@@ -18,14 +18,14 @@ int py_c_update_feature(int *train_points, int num_points, float *users,
 		
 		/* Calculate prediction error */
 		/* TODO: Use residuals to avoid repeat dot product calculations */
-		temp_user_val  = &(users[*user * num_features]);
-		temp_movie_val = &(movies[*movie * num_features]);
+		temp_user_val  = &users[*user * num_features];
+		temp_movie_val = &movies[*movie * num_features];
 		for(j = 0; j < num_features; j++){
 			temp_prediction += temp_user_val[j] * temp_movie_val[j];
 			
 		}
 		
-		error = rating - temp_prediction;
+		error = *rating - temp_prediction;
 		/* Update user and movie */
 		user_change  = learn_rate * error * movies[(*movie * num_features) + feature];
 		movie_change = learn_rate * error * users[(*user  * num_features) + feature];
