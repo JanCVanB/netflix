c923b354437e9b705776941aa3f9cbd3864b4fc5
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-04-24 22:29:48 -0700

    Add run_model.run_multi() and use that in run_svd, untested

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -13,27 +13,27 @@ def calculate_rmse(true_ratings, predictions):
 
 
 def run(model, train_set_name, test_set_name, epochs=None, features=None):
-    print('Training {modelclass} on "{train}" ratings'
-          .format(modelclass=model.__class__.__name__, train=train_set_name))
+    print('Training {model_class} on "{train}" ratings'
+          .format(model_class=model.__class__.__name__, train=train_set_name))
     if epochs is not None:
         print('Number of epochs:', epochs)
     if features is not None:
         print('Number of features:', features)
     time_format = '%b-%d-%Hh-%Mm'
     train_file_path = join(DATA_DIR_PATH, train_set_name + '.npy')
-    time_stamp = strftime(time_format, localtime())
 
     model.debug = True
     train_points = load_numpy_array_from_file(train_file_path)
+    times = strftime(time_format, localtime())
     model.train(train_points, epochs)
-    time_stamp += '_to_' + strftime(time_format, localtime())
+    times += '_to_' + strftime(time_format, localtime())
     model.train_points = None
 
     epochs_string = '' if epochs is None else ('_%sepochs' % epochs)
     features_string = '' if features is None else ('_%sfeatures' % features)
-    template_file_name = ('svd_{train}{e}{f}_xxx_{time}'
+    template_file_name = ('svd_{train}{e}{f}_xxx_{times}'
                           .format(train=train_set_name, e=epochs_string,
-                                  f=features_string, time=time_stamp))
+                                  f=features_string, times=times))
 
     model_file_name = template_file_name.replace('xxx', 'model') + '.p'
     model.save(model_file_name)
@@ -42,8 +42,9 @@ def run(model, train_set_name, test_set_name, epochs=None, features=None):
     test_file_path = join(DATA_DIR_PATH, test_set_name + '.npy')
     test_points = load_numpy_array_from_file(test_file_path)
     predictions = model.predict(test_points)
-    pred_file_name = template_file_name.replace('xxx', 'predictions') + '.dta'
-    save_predictions(predictions, pred_file_name)
+    predictions_file_name = (template_file_name.replace('xxx', 'predictions') +
+                             '.dta')
+    save_predictions(predictions, predictions_file_name)
 
     true_ratings = test_points[:, 3]
     rmse = calculate_rmse(true_ratings, predictions)
@@ -53,13 +54,54 @@ def run(model, train_set_name, test_set_name, epochs=None, features=None):
     save_rmse(rmse, rmse_file_name)
 
 
+def run_multi(model, train_set_name, test_set_name, epochs=None, features=None):
+    print('Training {model_class} on "{train}" ratings'
+          .format(model_class=model.__class__.__name__, train=train_set_name))
+    if epochs is not None:
+        print('Maximum number of epochs:', epochs)
+    if features is not None:
+        print('Number of features:', features)
+
+    train_file_path = join(DATA_DIR_PATH, train_set_name + '.npy')
+    train_points = load_numpy_array_from_file(train_file_path)
+    test_file_path = join(DATA_DIR_PATH, test_set_name + '.npy')
+    test_points = load_numpy_array_from_file(test_file_path)
+
+    epochs_string = '' if epochs is None else ('_%sepochs' % epochs)
+    features_string = '' if features is None else ('_%sfeatures' % features)
+    time_format = '%b-%d-%Hh-%Mm'
+    rmse_file_name = ('svd_{train}{e}{f}_rmse_{test}_{time}.txt'
+                      .format(train=train_set_name, test=test_set_name,
+                              e=epochs_string, f=features_string,
+                              time=strftime(time_format, localtime())))
+
+    model.debug = True
+    for epoch in range(epochs):
+        print('Training epoch {}:'.format(epoch))
+        if epoch == 1:
+            model.train(train_points, epochs=1)
+        else:
+            model.train_more(epochs=1)
+        print('Predicting "{test}" ratings'.format(test=test_set_name))
+        predictions = model.predict(test_points)
+        true_ratings = test_points[:, 3]
+        rmse = calculate_rmse(true_ratings, predictions)
+        print('RMSE:', rmse)
+        rmse_file_name = rmse_file_name
+        save_rmse(rmse, rmse_file_name, append=True)
+    model.train_points = None
+
+
 def save_predictions(predictions, predictions_file_name):
     predictions_file_path = join(RESULTS_DIR_PATH, predictions_file_name)
     with open(predictions_file_path, 'w+') as predictions_file:
         predictions_file.writelines(['{:.3f}\n'.format(p) for p in predictions])
 
 
-def save_rmse(rmse, rmse_file_name):
+def save_rmse(rmse, rmse_file_name, append=False):
     rmse_file_path = join(RESULTS_DIR_PATH, rmse_file_name)
-    with open(rmse_file_path, 'w+') as rmse_file:
-        rmse_file.write(str(rmse))
+    write_format = 'w+'
+    if append:
+        write_format = 'a+'
+    with open(rmse_file_path, write_format) as rmse_file:
+        rmse_file.write('{}\n'.format(rmse))
--- scripts/run_svd.py
+++ scripts/run_svd.py
@@ -3,14 +3,14 @@ import sys
 
 sys.path.append(abspath(dirname(dirname(__file__))))
 from algorithms.svd import SVD
-from scripts.run_model import run
+from scripts.run_model import run_multi
 
-NUMBER_OF_EPOCHS = 1
-NUMBER_OF_FEATURES = 1
-TRAIN_SET_NAME = 'valid'
-TEST_SET_NAME = 'hidden'
+NUMBER_OF_EPOCHS = 5
+NUMBER_OF_FEATURES = 5
+TRAIN_SET_NAME = 'base'
+TEST_SET_NAME = 'valid'
 
 model = SVD(num_features=NUMBER_OF_FEATURES)
 model.run_c = True
-run(model, TRAIN_SET_NAME, TEST_SET_NAME, epochs=NUMBER_OF_EPOCHS,
-    features=NUMBER_OF_FEATURES)
+run_multi(model, TRAIN_SET_NAME, TEST_SET_NAME,
+          epochs=NUMBER_OF_EPOCHS, features=NUMBER_OF_FEATURES)
