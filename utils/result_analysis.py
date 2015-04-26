28af79e1f4362db812247911b3cda1831bed75bf
qosha1 <qosha@caltech.edu>
2015-04-26 16:24:01 -0700

    Add script to find lowest rmse result values

---
+++ utils/result_analysis.py
@@ -0,0 +1,28 @@
+__author__ = 'quinnosha'
+
+"""Tools for viewing and analyzing prediction results
+
+.. moduleauthor:: Quinn Osha
+"""
+
+
+from os.path import abspath, dirname, join
+from utils.data_paths import RESULTS_DIR_PATH
+
+def find_lowest_rmse(rmse_file_name):
+    rmse_file_path = join(RESULTS_DIR_PATH, rmse_file_name)
+    read_format = 'r'
+    rmse_values = []
+
+    with open(rmse_file_path, read_format) as rmse_file:
+        for line in rmse_file:
+            rmse_value = line.strip();
+            rmse_values.append(rmse_value)
+
+    return min(rmse_values)
+
+
+if __name__ == '__main__':
+    rmse_file_name = 'svd_base_8epochs_100features__rmse_valid_Apr-26-12h-43m.txt'
+    lowest_rmse = find_lowest_rmse(rmse_file_name)
+    print(lowest_rmse)
