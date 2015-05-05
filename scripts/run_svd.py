839b5104e03651b4f29bd6f2c5848ef7ac42efca
Samuel Richerd <sricherd@caltech.edu>
2015-05-05 09:42:35 -0700

    Add run-time exception handling for run_svd.py to recover from a crash

--- scripts/run_svd.py
+++ scripts/run_svd.py
@@ -5,12 +5,16 @@ sys.path.append(abspath(dirname(dirname(__file__))))
 from algorithms.svd import SVD
 from scripts.run_model import run
 
-NUMBER_OF_EPOCHS = 20
-NUMBER_OF_FEATURES = 50
+NUMBER_OF_EPOCHS = 10
+NUMBER_OF_FEATURES = 40
 TRAIN_SET_NAME = 'base'
 TEST_SET_NAME = 'valid'
 
 model = SVD(num_features=NUMBER_OF_FEATURES)
 model.run_c = True
-run(model, TRAIN_SET_NAME, TEST_SET_NAME,
-    epochs=NUMBER_OF_EPOCHS, features=NUMBER_OF_FEATURES)
+try:
+    run(model, TRAIN_SET_NAME, TEST_SET_NAME,
+        epochs=NUMBER_OF_EPOCHS, features=NUMBER_OF_FEATURES)
+except Exception as exception:
+    import pdb
+    pdb.set_trace()
