9401b7bd44a781fc7cc8abeed1efbfa2f4cf2c5d
qosha1 <qosha@caltech.edu>
2015-05-06 01:19:32 -0700

    Squeeze just a bit more speed out of residuals. Remove repeated multiplications.

--- algorithms/svd.c
+++ algorithms/svd.c
@@ -91,7 +91,7 @@ int c_update_feature_with_residuals(int *train_points, int num_points, float *us
         int feature, int num_features){
 
 	int p, f;
-	float prediction, partial_prediction, feature_product;
+	float prediction, feature_product;
 	float *user_features, *movie_features;
 	float *user_features_cursor, *movie_features_cursor;
 	float error, user_change, movie_change;
@@ -107,10 +107,12 @@ int c_update_feature_with_residuals(int *train_points, int num_points, float *us
 
 		/* Calculate prediction error */
 		user_features  = users + (*user * num_features);
-	        movie_features = movies + (*movie * num_features);
-       		user_features_cursor  = user_features;
-      		movie_features_cursor = movie_features;
-        	prediction = 0;
+	    movie_features = movies + (*movie * num_features);
+       	user_features_cursor  = user_features;
+      	movie_features_cursor = movie_features;
+        prediction = 0;
+
+        feature_product = user_features[feature]*movie_features[feature];
 		if(feature == 0){
        		 for(f = 0; f < num_features; f++){
 	                prediction += *user_features_cursor * *movie_features_cursor;
@@ -122,10 +124,10 @@ int c_update_feature_with_residuals(int *train_points, int num_points, float *us
               	  //}         
   	 
 		}else{	
-		    partial_prediction = residuals[p];
-		    partial_prediction += user_features[feature-1]*movie_features[feature-1];
-                    feature_product = user_features[feature]*movie_features[feature]; 
-		    prediction = partial_prediction + feature_product;
+		    prediction  = residuals[p];
+		    prediction += user_features[feature-1]*movie_features[feature-1];
+            //feature_product = user_features[feature]*movie_features[feature];
+		    prediction += feature_product;
 		    //if(p == 0 || p == num_points-1){
                     //    printf("First prediction with residual:  %f\n", prediction);
 		    //	printf("Last/first previous user features: %f, movie features: %f\n",
@@ -148,19 +150,24 @@ int c_update_feature_with_residuals(int *train_points, int num_points, float *us
 		//		*user_features_cursor * *movie_features_cursor);	
 		//  printf("last element user change: %f and movie change: %f\n", user_change, movie_change);
 		//}
-        	/* mmm save the residual */
-		if(feature < num_features - 1){
-		  residuals[p] = prediction - *user_features_cursor * *movie_features_cursor;
-	          residuals[p] -= (*(user_features_cursor+1) * *(movie_features_cursor+1));
-		}
-		//if(p==num_points-1){
-		// printf("last element residual: %f, and prediction: %f\n", residuals[p], prediction);
-		//}
+
 		*user_features_cursor  += user_change;
 		*movie_features_cursor += movie_change;
 		//if(p==num_points-1){
 		// printf("last element updated user_feature: %f, movie_feature: %f\n", 
 		//	*user_features_cursor, *movie_features_cursor);
+		//}
+		user_features_cursor++; //next feature
+		movie_features_cursor++;
+
+      	/* mmm save the residual */
+		if(feature < num_features - 1){
+		    //residuals[p] = prediction - *user_features_cursor * *movie_features_cursor;
+		    residuals[p] = prediction - feature_product;
+	        residuals[p] -= (*(user_features_cursor) * *(movie_features_cursor));
+		}
+		//if(p==num_points-1){
+		// printf("last element residual: %f, and prediction: %f\n", residuals[p], prediction);
 		//}
 	}
 	//printf("First residual %f\n", residuals[0]);
